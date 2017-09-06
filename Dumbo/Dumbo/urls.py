cfd6ab03f95cae2b905d10ecabfcf7f4ad5c3553
chenjun <876765378@qq.com>
2017-09-06 20:27:31 +0800

    主页完成

--- Dumbo/Dumbo/urls.py
+++ Dumbo/Dumbo/urls.py
@@ -21,8 +21,12 @@ import os
 path = os.getcwd()
 path = path + '\Dumbo'
 sys.path.append(path)
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 from login import login_index
+from study import study_index
 from travel import travel_index
 from home import home_index
 from cooperation import product_info
@@ -32,10 +36,15 @@ from sport_delicacy import sport_delicacy_index
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', login_index.login),
+    url(r'^study/$', study_index.study),
     url(r'^home/$', home_index.home),
     url(r'^travel/$', travel_index.index),
     url(r'^travel/album$', travel_index.album),
     url(r'^product/$', product_info.product),
     url(r'^technology/$', technology_index.technology_info),
     url(r'^sport_delicacy/$', sport_delicacy_index.test),
+    url(r'^sport_delicacy/$', sport_delicacy_index.sport_delicacy),
+    # url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT}),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
